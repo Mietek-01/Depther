607071e22470acb3e544a37d7bde9dcda6fdb85d
Michał Niedek <43685643+Mietek-01@users.noreply.github.com>
2022-03-08 18:59:42 +0100

    Update Singleton.cs

--- Highlighted Scripts/Abstractions/Singleton.cs
+++ Highlighted Scripts/Abstractions/Singleton.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     protected static T instance;
 
